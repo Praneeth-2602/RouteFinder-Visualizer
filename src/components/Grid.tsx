8c3794e468a3925691a0412ab39df7ef91b658d5
Praneeth Palugula <praneeth.palugula@gmail.com>
2024-10-19 20:54:41 +0530

    added the file

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,4 +1,3 @@
-import { twMerge } from "tailwind-merge";
 import { usePathfinding } from "../hooks/usePathfinding";
 import { MAX_COLS, MAX_ROWS } from "../utils/constants";
 import { Tile } from "./Tile";
@@ -44,16 +43,12 @@ export function Grid({
 
     return (
         <div
-            className={twMerge(
-                // Base classes
-                "flex items-center flex-col justify-center border-sky-300 mt-10",
-                // Control Grid height
-                `lg:min-h-[${MAX_ROWS * 17}px]  md:min-h-[${MAX_ROWS * 15
-                }px] xs:min-h-[${MAX_ROWS * 8}px] min-h-[${MAX_ROWS * 7}px]`,
-                // Controlling grid width
-                `lg:w-[${MAX_COLS * 17}px] md:w-[${MAX_COLS * 15}px] xs:w-[${MAX_COLS * 8
-                }px] w-[${MAX_COLS * 7}px]`
-            )}
+            className= {`flex items-center flex-col justify-center border-sky-300 mt-10
+                lg:min-h-[${MAX_ROWS * 17}px]  md:min-h-[${MAX_ROWS * 15
+                }px] xs:min-h-[${MAX_ROWS * 8}px] min-h-[${MAX_ROWS * 7}px]
+                lg:w-[${MAX_COLS * 17}px] md:w-[${MAX_COLS * 15}px] xs:w-[${MAX_COLS * 8
+                }px] w-[${MAX_COLS * 7}px]`}
+            
         >
             {grid.map((r, rowIndex) => (
                 <div key={rowIndex} className="flex">
